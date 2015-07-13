6acb59bb0320be728713d349e1d0534df98fb984
Liz <lizelotte.green@gmail.com>
2015-07-13 09:22:31 -0500

    Created wiki model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708161027) do
+ActiveRecord::Schema.define(version: 20150711235457) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -26,5 +26,16 @@ ActiveRecord::Schema.define(version: 20150708161027) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
+
+  create_table "wikis", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "public"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
 
 end
